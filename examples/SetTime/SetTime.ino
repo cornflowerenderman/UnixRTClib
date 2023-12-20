0db79074fe9e363872cf2689a2bc072f2638f0c1
cornflowerenderman <99961948+cornflowerenderman@users.noreply.github.com>
2023-12-20 21:21:16 +1100

    Improve formatting

--- examples/SetTime/SetTime.ino
+++ examples/SetTime/SetTime.ino
@@ -8,17 +8,24 @@ void setup() {
   Serial.println("RTC Initialized");
   rtc.setTime(1703694200);  //Put the current unix timestamp here
                             //This function also enables the oscillator and clears the clock halt flag
-  printUnix(rtc.getTime());
-}
-
-void printUnix(uint64_t unixTime) {
-  Serial.print("Current unix timestamp: ");
-  uint32_t upper = unixTime / 1000000;
-  uint32_t lower = unixTime % 1000000;
-  Serial.print(upper);
-  Serial.print(' ');
-  Serial.println(lower);
+  print64bit(rtc.getTime());
+  Serial.println();
 }
 
 void loop() {
+}
+
+void print64bit(uint64_t number) {
+  if (number == 0) {
+    Serial.print('0');
+    return;
+  }
+  int8_t buffer[20];
+  uint8_t len = 0;
+  while (number > 0) {
+    uint64_t t = number / 10;
+    buffer[len++] = number - t * 10 + '0';
+    number = t;
+  }
+  for (; len > 0; len--) Serial.print((char)buffer[len - 1]);
 }
