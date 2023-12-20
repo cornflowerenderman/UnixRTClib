71783848addfdce89eb0fed1613a03f98eb41747
cornflowerenderman <99961948+cornflowerenderman@users.noreply.github.com>
2023-12-20 21:22:42 +1100

    Improve formatting

--- examples/TestY2100/TestY2100.ino
+++ examples/TestY2100/TestY2100.ino
@@ -8,23 +8,31 @@ void setup() {
   Serial.println("RTC Initialized");
   rtc.setTime(1777777777);  // May 03 2026 03:09:37
   delay(3000);
-  printUnix(rtc.getTime());
+  print64bit(rtc.getTime());
   rtc.setTime(4102444799);  // 31st dec 2099 23:59:59
   delay(3000);
-  printUnix(rtc.getTime());
+  print64bit(rtc.getTime());
   rtc.setTime(4107542399);  // Feb 28th Y2100 23:59:59
   delay(3000);
-  printUnix(rtc.getTime());
+  print64bit(rtc.getTime());
 }
-void printUnix(uint64_t unixTime) {
-  Serial.print("Current unix timestamp: ");
-  uint32_t upper = unixTime / 1000000;
-  uint32_t lower = unixTime % 1000000;
-  Serial.print(upper);
-  Serial.print(' ');
-  Serial.println(lower);
-}
+
 void loop() {
-  printUnix(rtc.getTime());
+  print64bit(rtc.getTime());
   delay(5000);
+}
+
+void print64bit(uint64_t number) {
+  if (number == 0) {
+    Serial.print('0');
+    return;
+  }
+  int8_t buffer[20];
+  uint8_t len = 0;
+  while (number > 0) {
+    uint64_t t = number / 10;
+    buffer[len++] = number - t * 10 + '0';
+    number = t;
+  }
+  for (; len > 0; len--) Serial.print((char)buffer[len - 1]);
 }
